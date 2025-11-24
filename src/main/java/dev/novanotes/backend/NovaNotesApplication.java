33df0b4ada9122a436f29003f1e940373e7840f5
Asmita <rgaa42002@gmail.com>
2025-11-24 12:49:17 +0530

    changed user and email for git

--- src/main/java/dev/novanotes/backend/NovaNotesApplication.java
+++ src/main/java/dev/novanotes/backend/NovaNotesApplication.java
@@ -2,8 +2,14 @@ package dev.novanotes.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "dev.novanotes.backend.entity")
+@EntityScan(basePackages = "dev.novanotes.backend.entity")
+@EnableTransactionManagement
 public class NovaNotesApplication {
 
 //	set JAVA_HOME=C:\Program Files\Java\jdk-17
