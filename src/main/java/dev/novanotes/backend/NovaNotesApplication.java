3b406cf41a7de1ac1c691608c875564d90d39eab
Asmita <logicsoft121@gmail.com>
2025-11-24 09:52:35 +0530

    Initial project setup

---
+++ src/main/java/dev/novanotes/backend/NovaNotesApplication.java
@@ -0,0 +1,14 @@
+package dev.novanotes.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaNotesApplication {
+
+//	set JAVA_HOME=C:\Program Files\Java\jdk-17
+	public static void main(String[] args) {
+		SpringApplication.run(NovaNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/novanotes/backend/NovaNotesApplicationTests.java
@@ -0,0 +1,13 @@
+//package dev.novanotes.backend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NovaNotesApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
